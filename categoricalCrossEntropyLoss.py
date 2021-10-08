fbc691034420d76d6d7a65de600ca5b2b2219f7b
Utkarsh736 <utkarshtomar736@gmail.com>
2021-10-08 08:44:36 +0530

    Initial Commit

---
+++ categoricalCrossEntropyLoss.py
@@ -0,0 +1,16 @@
+import math
+
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+
+loss = -(math.log(softmax_output[0])*target_output[0]+
+         math.log(softmax_output[1])*target_output[1]+
+         math.log(softmax_output[2])*target_output[2]
+         )
+
+print(loss)
+
+# Confidence(softmax_output[]) is low, loss is high and vice versa.
+
+print(-(math.log(0.7)))
+print(-math.log(0.5))
